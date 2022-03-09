84303bc4eae5ac1a82a361889837e0cdbb96f56f
Roshan Gauchan <rgauchan3@gmail.com>
2022-03-09 02:15:44 +0000

    uk

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -17,7 +17,7 @@ namespace ConsoleAppProject
     {
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
 
             Console.WriteLine();
             Console.WriteLine(" =================================================");
